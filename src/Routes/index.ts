2d01c231c14f0880f33b1c0b5579e159859d3d46
LuisESouza <leduardosouza2204@gmail.com>
2025-03-26 15:13:58 -0400

    FEAT: Adicionando autenticador nas rotas

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,10 +1,11 @@
-import express  from 'express';
-import user from './userRoutes';
 import transaction from './transactionsRoutes';
+import { auth } from '../middleware/auth';
+import user from './userRoutes';
+import express  from 'express';
 
 const routes = express.Router();
 
 routes.use('/user', user);
-routes.use('/transactions', transaction);
+routes.use('/transactions',auth, transaction);
 
 export default routes;
